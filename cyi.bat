1bcfb562063d4ef89c5f30dd089d386fae07615e
jayshiai <jayvardhanpatil7811@gmail.com>
2024-11-08 18:36:53 +0530

    removed files

--- cyi.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-D:\MinGW\mingw64\bin\g++.exe -g .\cyi\main.cpp .\CodeAnalysis\Syntax\*.cpp .\CodeAnalysis\Binder\*.cpp .\CodeAnalysis\Text\*.cpp .\CodeAnalysis\Symbol\*.cpp .\CodeAnalysis\Lowerer\*.cpp .\Utils\*.cpp -I include -o compiler
-if %errorlevel% equ 0 (
-    echo Build succeeded. Running the program...
-    compiler %*
-) else (
-    echo Build failed.
-)
